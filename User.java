597588a8825e6729fd99b4bd56c46bd417ecf06a
VanTho <ovantho094@gmail.com>
2025-04-26 09:27:53 +0700

    user and property classes
    user and property classes

--- User.java
+++ User.java
@@ -1,4 +1,23 @@
 public class User {
+    int idCounter = 0;
+    int userId;
+    String email;
+    String password;
+    String userType;
     
-    
+    public User(String email, String password, String userType){
+        this.userId = ++idCounter;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public boolean resetPassword(String curPassword, String newPassword){
+        if(!curPassword.equals(this.password)){
+            return false;
+        }
+
+        this.password = newPassword;
+        return true;
+    }
 }
